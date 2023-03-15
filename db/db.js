e0a806d07632f1a2ac3b730f3a18e46300e1d6ee
Muhammed sadique <mhdsadique8@gmail.com>
2023-03-15 18:22:18 +0530

    a123452

--- db/db.js
+++ db/db.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 
 const mongoose=require("mongoose")
-const connection=mongoose.connect(process.env.url)
+const connection=mongoose.connect(process.env.url,{useNewUrlParser:true,
+    useUnifiedTopology:true,useCreateIndex:true})
 
 module.exports={
     connection
